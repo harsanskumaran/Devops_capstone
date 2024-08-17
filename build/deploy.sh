67680f113d1b4072829d5893d1091208e5247ca1
HaKu <harsanskumaran@gmail.com>
2024-08-17 15:37:13 +0530

    has

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,10 +3,19 @@
 echo "Deploy.sh has started"
 echo "===========================>"
 docker-compose up -d
-echo "Deploy has completed"
+if [ $? -eq 0 ]; then
+     echo "Deploy has completed"
+else 
+     echo "Deploy failed"
+fi
 echo "===========================>"
 echo "tagging started"
 docker tag harsanskumaran/dev:v1 harsanskumaran/pro:v1
+if [ $? -eq 0 ]; then
+     echo "tagged"
+else 
+     echo "tagged failed"
+fi
 echo "===========================>"
 docker login
 docker push harsanskumaran/pro:v1
